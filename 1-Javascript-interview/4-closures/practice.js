90c293cabded87e2ac93e7a449627699584133ac
nanda <nandakishorelogu@gmail.com>
2026-03-03 00:39:03 +0530

    continue practice closures

--- 1-Javascript-interview/4-closures/practice.js
+++ 1-Javascript-interview/4-closures/practice.js
@@ -13,13 +13,57 @@
 
 // Question 2 - Write a function that would allow you to do this?
 
-function createBase(a) {
-  return function (b) {
-    return b + a;
-  };
+// function createBase(a) {
+//   return function (b) {
+//     return b + a;
+//   };
+// }
+
+// var addSix = createBase(6);
+
+// console.log(addSix(10)); // returns 16
+// console.log(addSix(21)); // returns 27
+
+// Question 3 - Time Optimization
+
+// function find() {
+//   let a = [];
+//   for (let i = 0; i < 1000000; i++) {
+//     a[i] = i * i;
+//   }
+
+//   return function (index) {
+//     console.log(a[index]);
+//   };
+// }
+
+// console.time("6");
+// const closure = find();
+// closure(6);
+// console.timeEnd("6");
+// console.time("50");
+// closure(50);
+// console.timeEnd("50");
+
+// Question 4 - Block Scope and setTimeout
+
+// function a() {
+//   for (var i = 0; i < 3; i++) {
+//     setTimeout(function log() {
+//       console.log(i);
+//     }, 1000);
+//   }
+// }
+
+// a(); // it will print 3 for 3 times
+
+// Follow up question then how will you 0 1 2 here using var variable itself
+
+for (var i = 0; i < 3; i++) {
+  function inner(i) {
+    setTimeout(function log() {
+      console.log(i);
+    }, 1000);
+  }
+  inner(i);
 }
-
-var addSix = createBase(6);
-
-console.log(addSix(10)); // returns 16
-console.log(addSix(21)); // returns 27
