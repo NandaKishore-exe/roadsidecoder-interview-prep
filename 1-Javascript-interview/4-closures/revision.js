4490126955e371ffcd200eded077dc2cd3f0e92b
nanda <nandakishorelogu@gmail.com>
2026-03-04 22:47:24 +0530

    started practising closures

--- 1-Javascript-interview/4-closures/revision.js
+++ 1-Javascript-interview/4-closures/revision.js
@@ -1,1 +1,14 @@
 // Need revision
+
+let count = 0;
+
+(function printCount() {
+  if (count === 0) {
+    let count = 1;
+    console.log(count);
+  }
+
+  console.log(count);
+})();
+
+// output : 1 0
