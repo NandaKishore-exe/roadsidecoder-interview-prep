a564dc7bdf318272898b506af3fc8184eeb29742
nanda <nandakishorelogu@gmail.com>
2026-03-26 22:24:43 +0530

    started promises

--- 1-Javascript-interview/9-promises/script.js
+++ 1-Javascript-interview/9-promises/script.js
@@ -1,22 +1,3 @@
 // Promises in Javascript
 
-console.log("start");
-
-const sub = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    const result = false;
-    if (result) {
-      resolve("You have been subscribed to our services");
-    } else {
-      reject(new Error("Sorry you haven't subscribed"));
-    }
-  }, 2000);
-});
-
-sub
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => console.error(err));
-
-console.log("end");
+// Question 1 - What is the output?
