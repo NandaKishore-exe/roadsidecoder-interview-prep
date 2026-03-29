721bbc4998a9e5f73a48f5ecb8686f09d8601b65
nanda <nandakishorelogu@gmail.com>
2026-03-29 22:32:27 +0530

    continue promises

--- 1-Javascript-interview/9-promises/script.js
+++ 1-Javascript-interview/9-promises/script.js
@@ -92,6 +92,45 @@
 
 */
 
+// function job(state) {
+//   return new Promise(function (resolve, reject) {
+//     if (state) {
+//       resolve("success");
+//     } else {
+//       reject("error");
+//     }
+//   });
+// }
+
+// let promise = job(true);
+
+// promise
+//   .then(function (data) {
+//     console.log(data);
+
+//     return job(false);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+
+//     return "Error Caught";
+//   })
+//   .then(function (data) {
+//     console.log(data);
+
+//     return job(true);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// Question 6 - What's the output?
+
+/*
+success
+
+*/
+
 function job(state) {
   return new Promise(function (resolve, reject) {
     if (state) {
@@ -102,24 +141,41 @@ function job(state) {
   });
 }
 
-let promise = job(true);
+let promise = job(true); // resolve("success")
 
 promise
   .then(function (data) {
-    console.log(data);
-
-    return job(false);
+    console.log(data); // success
+    return job(true);
   })
-  .catch(function (error) {
-    console.log(error);
-
-    return "Error Caught";
+  .then(function (data) {
+    if (data !== "victory") {
+      throw "Defeat";
+    }
+    return job(true);
   })
   .then(function (data) {
     console.log(data);
-
+  })
+  .catch(function (error) {
+    console.log(error); // Defeat
+    return job(false);
+  })
+  .then(function (data) {
+    console.log(data);
     return job(true);
   })
   .catch(function (error) {
-    console.log(error);
+    console.log(error); // error
+    return "Error Caught";
+  })
+  .then(function (data) {
+    console.log(data); // Error Caught
+    return new Error("test");
+  })
+  .then(function (data) {
+    console.log("sucess:", data.message); // Success: test
+  })
+  .catch(function (data) {
+    console.log("error:", data.message);
   });
