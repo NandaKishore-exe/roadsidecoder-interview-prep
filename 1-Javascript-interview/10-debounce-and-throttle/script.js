89d7cc81d97b11b8ad5080f2996d58a6cd30d878
nanda <nandakishorelogu@gmail.com>
2026-04-10 23:27:20 +0530

    completed debounce and throttle using lodash

--- 1-Javascript-interview/10-debounce-and-throttle/script.js
+++ 1-Javascript-interview/10-debounce-and-throttle/script.js
@@ -1,1 +1,36 @@
-console.log("hello from debounce and throttle");
+// Debouncing and Throttling in Javascript
+
+// Ques 1 - Create a button UI and add debounce as follows =>
+
+// --> Show "Button Pressed <X> Times" every time button is pressed
+// --> Increase "Triggered <Y> Times" count after 800ms of debounce
+
+const btn = document.querySelector(".increment-btn");
+const btnPress = document.querySelector(".increment-pressed");
+const count = document.querySelector(".increment-count");
+
+var pressedCount = 0;
+var triggerCount = 0;
+
+// const debounceCount = _.debounce(() => {
+//   count.innerHTML = ++triggerCount;
+// }, 800);
+
+// btn.addEventListener("click", () => {
+//   btnPress.innerHTML = ++pressedCount;
+//   debounceCount();
+// });
+
+// Ques 2 - Create a button UI and add throttle as follows =>
+
+// --> Show "Button Pressed <X> Times" every time button is pressed
+// --> Increase "Triggered <Y> Times" count after 800ms of throttle
+
+const throttledCount = _.throttle(() => {
+  count.innerHTML = ++triggerCount;
+}, 800);
+
+btn.addEventListener("click", () => {
+  btnPress.innerHTML = ++pressedCount;
+  throttledCount();
+});
