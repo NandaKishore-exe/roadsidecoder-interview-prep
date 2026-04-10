0b1c0dc72d6d5a4c2f37053f638a087cd52ac55a
nanda <nandakishorelogu@gmail.com>
2026-04-10 23:50:57 +0530

    completed debounce and throttle polyfills

--- 1-Javascript-interview/10-debounce-and-throttle/script.js
+++ 1-Javascript-interview/10-debounce-and-throttle/script.js
@@ -26,8 +26,52 @@ var triggerCount = 0;
 // --> Show "Button Pressed <X> Times" every time button is pressed
 // --> Increase "Triggered <Y> Times" count after 800ms of throttle
 
-const throttledCount = _.throttle(() => {
-  count.innerHTML = ++triggerCount;
+// const throttledCount = _.throttle(() => {
+//   count.innerHTML = ++triggerCount;
+// }, 800);
+
+// btn.addEventListener("click", () => {
+//   btnPress.innerHTML = ++pressedCount;
+//   throttledCount();
+// });
+
+// Ques - 3 create debounce() polyfill implementation
+
+// const myDebounce = (callback, delay) => {
+//   let timer;
+//   return function (...args) {
+//     if (timer) clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       callback(...args);
+//     }, delay);
+//   };
+// };
+
+// const debounceCount = myDebounce(() => {
+//   triggerCount += 1;
+//   count.innerHTML = triggerCount;
+// }, 800);
+
+// btn.addEventListener("click", () => {
+//   btnPress.innerHTML = ++pressedCount;
+//   debounceCount();
+// });
+
+// Ques - 4 create throttle() polyfill implementation
+
+const myThrottle = (callback, delay) => {
+  let last = 0;
+  return function (...args) {
+    let now = new Date().getTime();
+    if (now - last < delay) return;
+    last = now;
+    return callback(...args);
+  };
+};
+
+const throttledCount = myThrottle(() => {
+  triggerCount += 1;
+  count.innerHTML = triggerCount;
 }, 800);
 
 btn.addEventListener("click", () => {
